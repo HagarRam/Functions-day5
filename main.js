debd9b748e7908c69b6c02a444006b2a920da7a0
HagarRam <hagaram11@gmail.com>
2022-12-20 09:49:55 +0200

    final day5

--- main.js
+++ main.js
@@ -12,16 +12,13 @@ function degrees(celsius){
     return conversionToFahrenheit
 }
 //Write a function to print a random number.
-// Check out Math.random().
 function randomNum() {
     let x = Math.random();
     return x;
 }
 //Write a function named tellFortune that:
 //takes 4 arguments: number of children, partner's name, geographic location, job title.
-//Alert your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
-//Call that function 3 times with 3 different values for the arguments.
-
+//Alert your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids.
 function tellFortune(numberOfChildren, partnersName,gergraphicLocation, jobTitle){
     alert(`"You will be a ${jobTitle} in ${gergraphicLocation}, and married to ${partnersName} with ${numberOfChildren} kids.
     `);
@@ -31,19 +28,15 @@ tellFortune("doctor", "Gaya and Tal", "Bat Yam", "4");
 tellFortune("doctor", "Tali and DAN", "Haifa", "3");
  
 //Write a function that takes in a 7 digits number and returns the number as a string with commas as thousand separators.
-// For example, the number 1234567 should be returned as "1,234,567".
 function numberSeparators(x){
     return(x.toLocaleString());  
 }
 //Write a function that takes in a string and returns the string with all a,e,i,o,u removed.
-// For example, the string "Hello, World!" should be returned as "Hll, Wrd!".
 function removeVowels(x){
     return x.replace(/[aeiou]/gi,'');
 }
 console.log(removeVowels("aaedd"));
 //Write a function that takes in a number and returns the number rounded to the nearest integer
-//. For example, the number 4.6 should be returned as 5, and the number 4.4 should be returned as 4
-//. (check out Math methods and properties).
 function roundedNumber(x){
     let y = Math.round(x);
     console.log(y);
